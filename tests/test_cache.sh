45969ebf3d4484f0490ea14ad899ac153b360caa
dmitry <dmitry@dmitry-ThinkPad-W530.(none)>
2013-02-13 15:44:05 +0200

    Wrap with restarting server and output to result file

--- tests/test_cache.sh
+++ tests/test_cache.sh
@@ -2,13 +2,24 @@
 file_size=$1
 files_number=$2
 tests_number=$3
+OUT_PUT=$4
 
 function test_arg_conditions() {
-    if [[ -z $file_size  || -z $files_number || -z $tests_number ]];
+    if [[ -z $file_size  || -z $files_number || -z $tests_number || -z $OUT_PUT ]];
     then
-        echo "please use $0 [file size in megabytes] [files number to generate] [test repeat number]";
+        echo "please use $0 [file size in megabytes] [files number to generate] [test repeat number] [output result file name]";
         exit -1;
     fi
+}
+
+function restart_services() {
+    sudo service incredibuild stop
+    wait 1
+    sudo /etc/init.d/clean_incredibuild_log.sh
+    wait 1
+    sudo service incredibuild start
+    wait 1
+    firefox http://localhost:8080/incredibuild/monitor/default.html &
 }
 
 function generate_files() {
@@ -33,7 +44,28 @@ function delay() {
     sleep 10;
 }
 
+function print_test_results() {
+    cat << EOF > $OUT_PUT
+#####################################################################################################################################
+Internal small test for caching
+  - run $tests_number number of tests
+#####################################################################################################################################
+
+EOF
+    cat << EOF >> $OUT_PUT
+
+
+#####################################################################################################################################
+
+Incredibuild LOG:
+EOF
+    sudo sh -c "cat /var/log/incredibuild >> $OUT_PUT"
+
+}
+
+
 test_arg_conditions
+restart_services
 echo "Genreating files for tests"
 generate_files
 echo "Run fresh test"
@@ -43,3 +75,5 @@ for (( k=1; $k<=$tests_number; k=$k+1 )); do
     delay;
     run_md5_test;
 done
+print_test_results
+gedit $OUT_PUT &
