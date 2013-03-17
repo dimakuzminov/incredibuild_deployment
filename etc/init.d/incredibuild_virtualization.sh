8d0ac38196c5da24158f9f4d80e7c4cb3d10cb8a
Dmitry Kuzminov <dima@galilsoft.com>
2013-03-17 13:46:45 +0200

    change virtualization NFS params

--- etc/init.d/incredibuild_virtualization.sh
+++ etc/init.d/incredibuild_virtualization.sh
@@ -3,7 +3,8 @@ MACHINE_NAME=$1
 USERNAME=$2
 PASSWORD=$3
 MACHINE_NAME_ROOT=/tmp/$MACHINE_NAME
-CACHE_FS_OPTION="-o fsc,rw"
+#CACHE_FS_OPTION="-o fsc,rw,soft,intr,rsize=8192,wsize=8192,udp"
+CACHE_FS_OPTION="-o fsc,rw,soft,intr,rsize=32768,wsize=32768,udp,noatime"
 
 function create_root() {
     if [[ -d $MACHINE_NAME_ROOT ]];
