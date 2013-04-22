e2dffbcdb2135efc15ca48dd720ff6cebd9ae39d
Dmitry Kuzminov <dima@galilsoft.com>
2013-04-22 17:42:21 +0300

    Add new version, scheduler + multi mount support

--- etc/init.d/incredibuild_virtualization.sh
+++ etc/init.d/incredibuild_virtualization.sh
@@ -2,8 +2,9 @@
 MACHINE_NAME=$1
 USERNAME=$2
 PASSWORD=$3
+MOUNT_POINTS=$4
 MACHINE_NAME_ROOT=/tmp/$MACHINE_NAME
-CACHE_FS_OPTION="-o fsc,rw"
+CACHE_FS_OPTION="-o fsc,rw,soft,intr,rsize=32768,wsize=32768,udp,noatime"
 
 function create_root() {
     if [[ -d $MACHINE_NAME_ROOT ]];
@@ -19,8 +20,17 @@ function connect_remote() {
     result=$(mount | grep $MACHINE_NAME)
     if [ -z "$result" ];
     then
-        echo "Connecting to $MACHINE_NAME";
-        echo $PASSWORD | sudo -S mount $MACHINE_NAME:/ $MACHINE_NAME_ROOT $CACHE_FS_OPTION;
+        if [ -z "$MOUNT_POINTS" ];
+        then
+            echo "Connecting to $MACHINE_NAME,  No mount points";
+            echo $PASSWORD | sudo -S mount $MACHINE_NAME:/ $MACHINE_NAME_ROOT/ $CACHE_FS_OPTION;
+        else
+            echo "Connecting to $MACHINE_NAME,  Found mount points";
+            for i in $MOUNT_POINTS;
+            do
+                echo $PASSWORD | sudo -S mount $MACHINE_NAME:/$i $MACHINE_NAME_ROOT/$i $CACHE_FS_OPTION;
+            done
+        fi
     else
         echo "$MACHINE_NAME is already connected";
     fi
