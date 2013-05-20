36bb662f5fbdd78302960c728642eaead040c296
Dmitry Kuzminov <dima@galilsoft.com>
2013-05-20 14:29:33 +0300

    Fix helper machine script: remove obsolete code, add user condition checking

--- prepare_helper_machine.sh
+++ prepare_helper_machine.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 password=xoreax
 user=incredibuild
-virtualization_script=/etc/init.d/incredibuild_virtualization.sh
-tmp_virtualization_script=/tmp/virt.back
-grid_domain_file=$1
 script_name=$0
-http_repository=/var/www/incredibuild
 PROJECT_DIR=$(pwd)
-FILENAME=/etc/grid_server_domain.conf
-USERNAME=root
 SSH_ROOT_DIR=/root/.ssh
 PERM_FILE=$PROJECT_DIR/linux.pem
 
+function __wait() {
+    while [ -e /proc/$1 ]
+    do
+        echo -ne "."
+        sleep 1;
+    done
+    echo -ne " done"
+    echo ""
+}
 
 function check_conditions() {
-    if [ -z "$grid_domain_file" ];
+    if ! [ $(id -u) = 0 ];
     then
-        echo "Error, missing parameter";
-        echo "please run $script_name grid_server_domain.conf";
+        echo "Error, must be run in root mode";
+        echo "please run sudo $script_name";
         exit
     fi
 }
 
 function install_linux_packages() {
-    sudo apt-get update -qq
-    sudo apt-get install -qq -y \
+    echo -ne "Installing critical linux packages: .."
+    apt-get update -qq
+    apt-get install -qq -y \
         nfs-kernel-server cachefilesd libssh-dev boa ssh
-    sudo sed "s;\<Port 80\>;Port 8080;" -i /etc/boa/boa.conf
+    sed "s;\<Port 80\>;Port 8080;" -i /etc/boa/boa.conf
 }
 
 function enable_cachefs() {
-    sudo sed "s;\# RUN;RUN;" -i /etc/default/cachefilesd
+    sed "s;\# RUN;RUN;" -i /etc/default/cachefilesd
 }
 
 function setup_nfs() {
     found=$(grep "/ \*(" /etc/exports)
     if [ -z "$found" ];
     then
-        sudo sh -c 'echo "/ *(rw,sync,no_subtree_check)" >> /etc/exports';
-        sudo exportfs -ra;
+        echo "/ *(rw,sync,no_subtree_check)" >> /etc/exports;
+        exportfs -ra;
     fi
 }
 
 function set_user_env() {
-    sudo deluser $user
-    echo "$password
+    if [ $(grep $user /etc/passwd) ];
+    then
+        echo "user $user already exists, skipping";
+    else
+        echo "$password
 $password
 
 
@@ -52,61 +59,49 @@ $password
 
 
 y
-" | sudo adduser $user
-    sudo adduser $user root
-    sudo adduser $user sudo
-    found=$(sudo grep "$user" /etc/sudoers)
+" | adduser $user;
+        adduser $user root;
+        adduser $user sudo;
+    fi
+    found=$(grep "$user" /etc/sudoers)
     if [ -z "$found" ];
     then
-        sudo sh -c "echo '%$user ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers";
+        echo '%$user ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers;
+    else
+        echo "user $user already sudo, skipping"
     fi
 }
 
 function copy_system_files() {
-    sudo sh -c "cp -fr etc/* /etc/"
-    sudo sh -c "cp -fr bin/* /bin/"
-    sudo sh -c "cp -fr $grid_domain_file /etc/grid_server_domain.conf"
-}
-
-function copy_web_files() {
-    sudo mkdir -p $http_repository 
-    sudo sh -c "cp -fr web/* $http_repository/"
-}
-
-function restart_services() {
-    sudo service rsyslog stop
-    sudo service rsyslog start
-    sudo service boa stop
-    sudo service boa start
+    cp -fr etc/* /etc/
 }
 
 function create_ssh_root() {
-    sudo mkdir -p $SSH_ROOT_DIR
+    mkdir -p $SSH_ROOT_DIR
 }
 
 function create_config_file() {
-    sudo sh -c "echo 'IdentityFile ~/.ssh/ids/%h/%r/id_rsa' > $SSH_ROOT_DIR/config"
-    sudo sh -c "echo 'IdentityFile ~/.ssh/ids/%h/%r/id_dsa' >> $SSH_ROOT_DIR/config"
-    sudo sh -c "echo 'IdentityFile ~/.ssh/ids/%h/id_rsa' >> $SSH_ROOT_DIR/config"
-    sudo sh -c "echo 'IdentityFile ~/.ssh/ids/%h/id_dsa' >> $SSH_ROOT_DIR/config"
-    sudo sh -c "echo 'IdentityFile ~/.ssh/id_rsa' >> $SSH_ROOT_DIR/config"
-    sudo sh -c "echo 'IdentityFile ~/.ssh/id_dsa' >> $SSH_ROOT_DIR/config"
+    echo 'IdentityFile ~/.ssh/ids/%h/%r/id_rsa' > $SSH_ROOT_DIR/config
+    echo 'IdentityFile ~/.ssh/ids/%h/%r/id_dsa' >> $SSH_ROOT_DIR/config
+    echo 'IdentityFile ~/.ssh/ids/%h/id_rsa' >> $SSH_ROOT_DIR/config
+    echo 'IdentityFile ~/.ssh/ids/%h/id_dsa' >> $SSH_ROOT_DIR/config
+    echo 'IdentityFile ~/.ssh/id_rsa' >> $SSH_ROOT_DIR/config
+    echo 'IdentityFile ~/.ssh/id_dsa' >> $SSH_ROOT_DIR/config
 }
 
 function create_domain_keys { 
-    sudo chmod 0600 $PERM_FILE 
-    sudo mkdir -p $SSH_ROOT_DIR
-    sudo sh -c "ssh-keygen -y -f $PERM_FILE > $SSH_ROOT_DIR/authorized_keys"
+    chmod 0600 $PERM_FILE 
+    mkdir -p $SSH_ROOT_DIR
+    ssh-keygen -y -f $PERM_FILE > $SSH_ROOT_DIR/authorized_keys
 }
 
 check_conditions
-install_linux_packages
+install_linux_packages &
+__wait `jobs -p`
 setup_nfs
 enable_cachefs
 set_user_env
 copy_system_files
-copy_web_files
-restart_services
 
 echo "Set security system for BuildMachine"
 create_ssh_root
