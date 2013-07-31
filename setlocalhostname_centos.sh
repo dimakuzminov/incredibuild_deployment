0f53727b0b1bdb9737aa947cee2d8ad5d5d92a22
dima <dima@centos-2.local>
2013-07-31 17:20:16 +0300

    Adding initial state script for setting hostname on centos

---
+++ setlocalhostname_centos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+NEW_NAME=$1
+AVAHI_PACKAGE="avahi-compat-howl.x86_64 \
+avahi-compat-libdns_sd.x86_64 \
+avahi-dnsconfd.x86_64 \
+avahi-tools.x86_64 \
+avahi-ui-tools.x86_64"
+
+function test_arg_conditions() {
+    if [[ -z $NEW_NAME ]];
+    then
+        echo "please use $0 [newhostname]";
+        exit -1;
+    fi
+}
+
+function check_avahi_package() {
+    if ! [[ -f /etc/init.d/avahi-daemon ]];
+    then
+        yum install -y $AVAHI_PACKAGE;
+        rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms;
+        rpm -ivh http://dl.atrpms.net/all/atrpms-repo-6-6.el6.x86_64.rpm;
+        cd /etc/yum.repos.d;
+        sed -i 's/enabled=0/enabled=1/g' atrpms.repo;
+        yum --enablerepo=atrpms install -y nss-mdns;
+    fi
+}
+
+function set_new_hostname() {
+    oldname=$(grep "127.0.0.1" /etc/hosts | awk '{print $2;}')
+    sudo sed "s;$oldname;$NEW_NAME;" -i /etc/hosts
+    sudo sh -c "echo $NEW_NAME > /etc/hostname"
+    sudo hostname -F /etc/hostname
+    echo "Need to reboot machine ...."
+    sudo reboot
+}
+
+test_arg_conditions
+check_avahi_package
+#set_new_hostname
